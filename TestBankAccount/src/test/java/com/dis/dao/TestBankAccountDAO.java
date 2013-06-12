e1b629444113e27bec68c83ed091b20fc0858fc0
unknown <dungpx@JAVA-DUNGPX-PC.qsoft.com.vn>
2013-06-12 14:31:03 +0700

    add structure for test

---
+++ TestBankAccount/src/test/java/com/dis/dao/TestBankAccountDAO.java
@@ -0,0 +1,15 @@
+package com.dis.dao;
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dungpx
+ * Date: 6/12/13
+ * Time: 2:16 PM
+ */
+
+public class TestBankAccountDAO {
+    BankAccountDAO mockBankAccountDAO = mock(BankAccountDAO.class);
+
+}
